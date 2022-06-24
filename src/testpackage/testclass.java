f06c9d7219fd688bfd903cce2c70a3a5059ee22d
Triple Baconator <triple@baconator.on.ca>
2022-06-23 20:44:29 -0400

    this is test

---
+++ src/testpackage/testclass.java
@@ -0,0 +1,11 @@
+package testpackage;
+
+public class testclass {
+
+	public static void main(String[] args) {
+		System.out.println("this is last test");
+		// TODO Auto-generated method stub
+
+	}
+
+}
